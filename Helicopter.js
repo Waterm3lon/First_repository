25234915d13ac954245d2b4b701bd73c90c97858
Sebastienne <Sebastienneprogramming@gmail.com>
2022-08-24 22:29:59 -0500

    Helicopter

---
+++ Helicopter.js
@@ -0,0 +1,17 @@
+
+let godtiervid = "https://www.youtube.com/watch?v=RoYs0avuhPY"
+
+function myfunction() {
+    let x = [1,2,3,7,11,34,21];
+    console.log(`I can count! ${x}`);
+}
+
+myfunction();
+
+if(__name__ == '__main__') {
+    while(1==1){
+        console.log("https://www.youtube.com/watch?v=RoYs0avuhPY")
+    }
+}
+//helicopter
+
