4af1cab5c71691b3eda6890fd866dc46a3a01897
Tony Hauber <thauber@gmail.com>
2022-08-30 17:05:01 -0500

    Format clean up, rename variables to camel case

--- Helicopter.js
+++ Helicopter.js
@@ -1,16 +1,15 @@
 
-let godtiervid = "https://www.youtube.com/watch?v=RoYs0avuhPY";
+let godTierVid = "https://www.youtube.com/watch?v=RoYs0avuhPY";
 
-function myfunction() {
+function myFunction() {
     let x = [1,2,3,7,11,34,21];
     console.log(`I can count! ${x}`);
 }
 
-myfunction();
+myFunction();
 
 while(1==1){
-        console.log(godtiervid);
-    };
+    console.log(godTierVid);
+};
 
 //helicopter
-
